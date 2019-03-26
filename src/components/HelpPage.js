0f18f6f3bb32b13a8a8933d61d378fd92cd159d5
Thein Htut <theinhtut.dec@gmail.com>
2019-03-26 22:04:38 +0800

    Setup Redux

---
+++ src/components/HelpPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HelpPage = () => (
+    <div>This is from my edit expense component.</div>
+);
+
+export default HelpPage;
---
+++ src/playground/destructuring.js
@@ -0,0 +1,12 @@
+const person = {
+  name: 'Thein',
+  age: 25,
+  location: {
+    city: 'Philadelphia',
+    temp: 92
+  }
+};
+
+const { name, age } = person;
+
+console.log(`${name} is ${age}.`);
