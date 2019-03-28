493f4d3de3b2b79dca65af83e79fab58b066539a
Thein Htut <theinhtut.dec@gmail.com>
2019-03-28 22:05:56 +0800

    Destructing to each js file

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Gas Bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const VisibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(VisibleExpenses);
+
 
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,18 +1,39 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy =
-        typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
+    // case 'DECREMENT':
+    //   const decrementBy =
+    //     typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+    //   return {
+    //     count: state.count - decrementBy
+    //   };
     case 'DECREMENT':
-      const decrementBy =
-        typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case 'RESET':
       return {
@@ -25,27 +46,31 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// });
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+store.dispatch(incrementCount({ incrementBy :5 }));
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy :10 }));
+
+store.dispatch(resetCount());
+
+// store.dispatch({
+//   type: 'RESET'
+// });
+
+store.dispatch(setCount({ count: 101 }));
