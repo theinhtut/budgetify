3c0d150ba15a3fa97135f3ea0edb0d8b4d276a30
Thein Htut <theinhtut.dec@gmail.com>
2019-04-05 15:51:23 +0800

    Added expenses summary component

--- src/components/BudgetifyDashboardPage.js
+++ src/components/BudgetifyDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpensesList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from '../components/ExpensesSummary';
 
 const BudgetifyDashboardPage = () => (
     <div>
+        <ExpensesSummary/>
         <ExpenseListFilters/>
         <ExpenseList/>
     </div>
