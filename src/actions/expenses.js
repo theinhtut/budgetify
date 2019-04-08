fe27e49a7c70d4fbde2be6b45569bd2573ea9181
Thein Htut <theinhtut.dec@gmail.com>
2019-04-08 12:41:48 +0800

    Added startRemoveExpense and startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -45,13 +45,29 @@ export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
+
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+        dispatch(removeExpense({ id }));
+    });
+  };
+};
   
   // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
-    type: 'EDIT_EXPENSE',
+    type: 'EDIT_EXPENSES',
     id,
     updates
 });
+
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+        dispatch(editExpense(id, updates));
+    });
+  };
+};
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
@@ -75,3 +91,4 @@ export const startSetExpenses = () => {
         });
     };
 };
+
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,8 +1,7 @@
 import moment from 'moment';
 // Get Visible Expenses
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
-  return expenses
-    .filter(expense => {
+  return expenses.filter((expense) => {
       // const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
       // const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
       const createdAtMoment = moment(expense.createdAt);
