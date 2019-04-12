e20f90ed4af27c6e4d5bbd1dc9efbcf370c29b98
Thein Htut <theinhtut.dec@gmail.com>
2019-04-12 12:47:43 +0800

    Added Styles For LoginPage, Header & SummaryPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,14 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Budgetify</h1>
+            <p>Manage your daily expenses 💸 at your fingertip.</p>
+            <button className="button" onClick={startLogin}>Login in with <i class="fab fa-google"></i></button>
+        </div>
+        <div className="box-layout__overlay"></div>
     </div>
 );
 
